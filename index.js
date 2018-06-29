4c2add0a3fd0a25786a7bffbbb145cc85baff8b6
Mariya <mariyawhitcomb@gmail.com>
2018-06-29 09:34:22 -0400

    build stack and queue on linked list

--- index.js
+++ index.js
@@ -32,8 +32,47 @@ newstack.push('hello')
 newstack.push("world")
 newstack.pop()
 newstack.peek()
-
-
-
-
 console.log(newstack)
+
+
+class Queue {
+    constructor(){
+        this.front = null
+        this.back = null
+    }
+    enqueue(element){
+        var N = new Node(element, null)
+        if (this.back === null){
+            this.front = N
+            this.back = N
+        }else{
+            this.back.next = N
+            this.back = this.back.next
+        }
+    }
+    dequeue(){
+        if(this.front !== null){
+            var first = this.front
+            this.front = this.front.next
+            return first.data
+        }else{
+            if(this.back !== null){
+                this.back = null
+            }
+        }
+    }
+    peek(){
+        return this.front.data
+    }
+}
+var newQueue = new Queue()
+
+newQueue.enqueue(20)
+newQueue.enqueue(30)
+newQueue.enqueue(40)
+newQueue.enqueue(50)
+newQueue.dequeue()
+newQueue.peek()
+var peek = newQueue.peek()
+console.log(peek)
+console.log(newQueue)
